d7710a358b236676d2bfc6ceb9c6d70978c2651e
KaieChen <63878759+KaieChen@users.noreply.github.com>
2020-04-26 16:14:56 -0700

    Update Bullet.java

--- InsertCoolName/src/Bullet.java
+++ InsertCoolName/src/Bullet.java
@@ -2,61 +2,43 @@ import java.awt.Rectangle;
 
 import processing.core.PApplet;
 
-public class Bullet extends GameObject implements Movable {
-	
-	private float velX, velY;
-	private static float maxVel = 5;
+public abstract class Bullet extends GameObject {
+	protected float velX, velY;
+	protected float locx;
+	protected float locy;
+	protected float angle;
+	protected float speed;
+	protected float damage;
+	protected float fireRate;
 
-	public Bullet(float x, float y, float angle, String name) {
+	public Bullet(float x, float y, float angle, float speed, float damage, float fireRate, String name) {
 		super(x, y, name);
-		velX = 1; 
-		velY = 1;
+		locx = x;
+		locy = y;
+		this.angle = angle;
+		this.speed = speed;
+		this.damage = damage;
+		this.fireRate = fireRate;
 		// TODO Auto-generated constructor stub
 	}
 
+	@Override
+	public abstract void act();
+
 	public float getVelX() {
-		// TODO Auto-generated method stub
 		return velX;
 	}
 
 	public float getVelY() {
-		// TODO Auto-generated method stub
 		return velY;
 	}
 
 	public void setVelX(float velX) {
-		// TODO Auto-generated method stub
 		this.velX = velX;
 	}
 
 	public void setVelY(float velY) {
-		// TODO Auto-generated method stub
 		this.velY = velY;
-	}
-
-	@Override
-	public void act() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void drawSelf(float x, float y, float tileWidth, float tileHeight, PApplet s) {
-		s.fill(0, 255, 0);
-		s.rect(x, y, 20, 20);
-		
-	}
-
-	@Override
-	public void onInteract(GameObject obj) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public Rectangle getBounds() {
-		// TODO Auto-generated method stub
-		return null;
 	}
 
 }
