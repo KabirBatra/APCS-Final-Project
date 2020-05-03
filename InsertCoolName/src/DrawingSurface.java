ae755c3b93d450d0358827007072f72a2078a46e
KaieChen <63878759+KaieChen@users.noreply.github.com>
2020-05-02 19:23:24 -0700

    Update DrawingSurface.java
    Add the options

--- InsertCoolName/src/DrawingSurface.java
+++ InsertCoolName/src/DrawingSurface.java
@@ -1,3 +1,10 @@
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
 import processing.core.PApplet;
 
 public class DrawingSurface extends PApplet {
@@ -6,7 +13,16 @@ public class DrawingSurface extends PApplet {
 	
 	public static void main(String[] args) {
 		System.out.println("hello world");
+		
+		JFrame frame = new JFrame();
+	      JPanel panel = new JPanel();
+
+	 int result =  JOptionPane.showConfirmDialog(null, "Play Game?", "REVENGE OF THE TOLIET PAPER", JOptionPane.YES_NO_OPTION);
+	      
+	if (result == JOptionPane.YES_OPTION)
 		PApplet.main("DrawingSurface");
+	else 
+		System.out.println("BYE");
 	}
 	
 	public DrawingSurface() {
