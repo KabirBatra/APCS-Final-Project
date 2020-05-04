ff74d12f5c12e3666f44025143f8bf615231c7c4
KaieChen <63878759+KaieChen@users.noreply.github.com>
2020-05-03 21:13:17 -0700

    Update Enemy.java
    Made it so that it would also be able to shoot and have a max speed

--- InsertCoolName/src/Enemy.java
+++ InsertCoolName/src/Enemy.java
@@ -3,10 +3,15 @@ import processing.core.PApplet;
 
 public class Enemy extends Creature {
 
-	public Enemy(float x, float y, String name, SpriteSheet ss) {
+	private GameHandler handler;
+	private float maxSpeed;
+	
+	public Enemy(float x, float y, float speed, GameHandler handler, String name, SpriteSheet ss) {
 		super(x, y, name, ss);
+		this.handler = handler;
 		velX = 1;
 		velY = 1;
+		maxSpeed = speed;
 	}
 
 	public void update(float ellapsedTime) {
@@ -25,6 +30,36 @@ public class Enemy extends Creature {
 	public void onInteract(GameObject obj) {
 		
 	}
+	public void shoot(PApplet s) {
+		Player closestPlayer = null;
+		float closestDistanceSquared = -1;
+		float distanceSquared;
+		for(GameObject obj : handler.objects) {
+			if(obj instanceof Player) {
+				
+				distanceSquared = (posX - obj.posX)*(posX - obj.posX) + (posY - obj.posY)*(posY - obj.posY);
+				
+				if(closestPlayer == null || distanceSquared < closestDistanceSquared) {
+					closestPlayer = (Player)obj;
+					closestDistanceSquared = distanceSquared;
+				}
+			}
+		}
+		float angle;
 
+		if(closestPlayer != null) {
+			angle = (float)Math.atan((posY - closestPlayer.posY)/(posX - closestPlayer.posX));
+			if(posX > closestPlayer.posX) {
+				angle+=Math.PI;
+			}
+		} else {
+			angle = 0; // shoot in the direction moving (using enum's index?)
+		}
+		handler.addGameObject(new NormalBullet(handler.getPlayer().getPosX()+0.5f, handler.getPlayer().getPosY()+0.5f, angle, "308"));
+	}
+
+	public float getMaxSpeed() {
+		return maxSpeed;
+	}
 
 }
